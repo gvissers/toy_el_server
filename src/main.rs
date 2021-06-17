3ef449c7db766e4cff8fa7eedf0a59076602cc2f
Gé Vissers <gvissers@gmail.com>
2021-06-17 09:49:45 +0200

    Handle login attempts (with an error message)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,179 @@
+use openssl::ssl::{SslConnector, SslMethod};
 use std::io::Read;
 use std::io::Write;
+
+struct Client
+{
+    socket: std::net::TcpStream
+}
+
+impl Client
+{
+    fn new(socket: std::net::TcpStream) -> Self
+    {
+        Client { socket }
+    }
+
+    fn send(&mut self, cmd: ClientCommand, data: &[u8]) -> Result<(), String>
+    {
+        let len = data.len() as u16 + 1;
+        let mut msg = vec![cmd as u8];
+        msg.push(len as u8);
+        msg.push((len >> 8) as u8);
+        msg.extend_from_slice(data);
+        self.socket.write_all(&msg).map_err(|err| format!("Failed to send data: {}", err))
+    }
+
+    fn send_text(&mut self, channel: ChatChannel, text: &str) -> Result<(), String>
+    {
+        let mut msg = vec![channel as u8];
+        // Taking a shortcut here, assuming text is strict ascii
+        msg.extend_from_slice(text.as_bytes());
+        self.send(ClientCommand::RawText, &msg)
+    }
+
+    fn handle_version(&mut self, data: &[u8]) -> Result<(), String>
+    {
+        if data.len() != 14
+        {
+            Err(format!("Invalid version packet length {}", data.len()))
+        }
+        else
+        {
+            let protocol_major = ((data[1] as u16) << 8) | data[0] as u16;
+            let protocol_minor = ((data[3] as u16) << 8) | data[2] as u16;
+            let client_major = data[4] as u8;
+            let client_minor = data[5] as u8;
+            let client_release = data[6] as u8;
+            let client_build = data[7] as u8;
+            let ip = &data[8..12];
+            let port = ((data[13] as u16) << 8) | data[12] as u16;
+            // Let's assume the client set the IP address and port number correctly
+            println!("Got {:?}, protocol = {}.{}, client = {}.{}.{}.{}, IP = {}.{}.{}.{}, port = {}",
+                ServerCommand::SendVersion,
+                protocol_major, protocol_minor,
+                client_major, client_minor, client_release, client_build,
+                ip[0], ip[1], ip[2], ip[3], port);
+
+            if protocol_major >= 11
+            {
+                // Let's see if the client wants to encrypt the connection
+                self.send(ClientCommand::LetsEncrypt, &[]);
+            }
+            Ok(())
+        }
+    }
+
+    fn handle_login(&mut self, data: &[u8]) -> Result<(), String>
+    {
+        let len_data = data.len();
+        if len_data == 0 || data[len_data-1] != 0
+        {
+            Err(String::from("Invalid login data"))
+        }
+        else
+        {
+            let mut it = data[..len_data-1].split(|&b| b == b' ');
+            if let Some(username) = it.next()
+            {
+                if let Some(password) = it.next()
+                {
+                    println!("Log in {} with a {}-byte password", to_utf8(username), password.len());
+                    self.send(ClientCommand::LogInNotOk, "Sorry, the Toy server does not accept logins".as_bytes());
+                    Ok(())
+                }
+                else
+                {
+                    Err(String::from("No password in login attempt"))
+                }
+            }
+            else
+            {
+                Err(String::from("No username in login attempt"))
+            }
+        }
+    }
+
+    fn handle_message(&mut self, code: u8, data: &[u8]) -> Result<(), String>
+    {
+        match get_command(code)
+        {
+            None => { eprintln!("Got unknown opcode {}", code); },
+            Some(cmd) => {
+                match cmd
+                {
+                    ServerCommand::RawText => { println!("Got {:?}: {}", cmd, to_utf8(data)); },
+                    ServerCommand::SendOpeningScreen => {
+                        println!("Got {:?}", cmd);
+                        // Looks like this may be ignored by the official server, but here we go:
+                        self.send_text(ChatChannel::Server, "Welcome to the Toy Server!")?;
+                    },
+                    ServerCommand::SendVersion => { self.handle_version(data)?; },
+                    ServerCommand::HeartBeat => { println!("Got {:?}", cmd); }
+                    ServerCommand::LetsEncrypt => { /* ignore for now */ },
+                    ServerCommand::LogIn => { self.handle_login(data)?; }
+                }
+            }
+        }
+        Ok(())
+    }
+
+    fn handle_packets(&mut self, buffer: &mut [u8]) -> Result<usize, String>
+    {
+        let mut offset = 0;
+        while offset + 3 <= buffer.len()
+        {
+            let rem = &buffer[offset..];
+            let msg_len = 2 + ((rem[2] as usize) << 8) + rem[1] as usize;
+            if msg_len < 3 || msg_len > buffer.len()
+            {
+                return Err(String::from("Invalid message length"));
+            }
+            if msg_len > rem.len()
+            {
+                break;
+            }
+
+            let code = rem[0];
+            let data = &rem[3..msg_len];
+            self.handle_message(code, data)?;
+
+            offset += msg_len;
+        }
+        let new_size = buffer.len() - offset;
+        buffer.copy_within(offset.., 0);
+
+        Ok(new_size)
+    }
+
+    fn handle_traffic(mut self)
+    {
+        let mut buffer = [0u8; 8192];
+        let mut buffer_used = 0;
+        loop
+        {
+            match self.socket.read(&mut buffer[buffer_used..])
+            {
+                Ok(0) => {
+                    println!("Client shut down the connection");
+                    break;
+                },
+                Ok(nr_bytes) => {
+                    buffer_used += nr_bytes;
+                    match self.handle_packets(&mut buffer[..buffer_used])
+                    {
+                        Ok(len) => { buffer_used = len; },
+                        Err(err) => { eprintln!("Protocol error: {}", err); break; }
+                    }
+                },
+                Err(err) => {
+                    eprintln!("Failed to read from client socket: {}", err);
+                    break;
+                }
+            }
+        }
+    }
+}
 
 #[derive(Clone, Copy, Debug)]
 enum ServerCommand
@@ -8,13 +182,16 @@ enum ServerCommand
     SendOpeningScreen = 9,
     SendVersion,
     HeartBeat = 14,
-    Invalid
+    LetsEncrypt = 101,
+    LogIn = 140
 }
 
 #[derive(Clone, Copy)]
 enum ClientCommand
 {
-    RawText
+    RawText,
+    LetsEncrypt = 101,
+    LogInNotOk = 251
 }
 
 #[derive(Clone, Copy)]
@@ -32,28 +209,17 @@ enum ChatChannel
     Popup = 0xff
 }
 
-const CMD_LUT: [ServerCommand; 16] = [
-    ServerCommand::RawText,
-    ServerCommand::Invalid,
-    ServerCommand::Invalid,
-    ServerCommand::Invalid,
-    ServerCommand::Invalid,
-    ServerCommand::Invalid,
-    ServerCommand::Invalid,
-    ServerCommand::Invalid,
-    ServerCommand::Invalid,
-    ServerCommand::SendOpeningScreen,
-    ServerCommand::SendVersion,
-    ServerCommand::Invalid,
-    ServerCommand::Invalid,
-    ServerCommand::Invalid,
-    ServerCommand::HeartBeat,
-    ServerCommand::Invalid,
-];
-
 fn get_command(code: u8) -> Option<ServerCommand>
 {
-    ((code as usize) < CMD_LUT.len()).then(|| CMD_LUT[code as usize])
+    match code
+    {
+          0 => Some(ServerCommand::RawText),
+          9 => Some(ServerCommand::SendOpeningScreen),
+         10 => Some(ServerCommand::SendVersion),
+         14 => Some(ServerCommand::HeartBeat),
+        140 => Some(ServerCommand::LogIn),
+          _ => None
+    }
 }
 
 fn to_utf8(text: &[u8]) -> String
@@ -135,131 +301,9 @@ fn to_utf8(text: &[u8]) -> String
     res
 }
 
-fn send(client: &mut std::net::TcpStream, cmd: ClientCommand, data: &[u8]) -> Result<(), String>
-{
-    let len = data.len() as u16 + 1;
-    let mut msg = vec![cmd as u8];
-    msg.push(len as u8);
-    msg.push((len >> 8) as u8);
-    msg.extend_from_slice(data);
-    client.write_all(&msg).map_err(|err| format!("Failed to send data: {}", err))
-}
-
-fn send_text(client: &mut std::net::TcpStream, channel: ChatChannel, text: &str) -> Result<(), String>
-{
-    let mut msg = vec![channel as u8];
-    // Taking a shortcut here, assuming text is strict ascii
-    msg.extend_from_slice(text.as_bytes());
-    send(client, ClientCommand::RawText, &msg)
-}
-
-fn handle_version(data: &[u8]) -> Result<(), String>
-{
-    if data.len() != 14
-    {
-        Err(format!("Invalid version packet length {}", data.len()))
-    }
-    else
-    {
-        let protocol_major = ((data[1] as u16) << 8) | data[0] as u16;
-        let protocol_minor = ((data[3] as u16) << 8) | data[2] as u16;
-        let client_major = data[4] as u8;
-        let client_minor = data[5] as u8;
-        let client_release = data[6] as u8;
-        let client_build = data[7] as u8;
-        let ip = &data[8..12];
-        let port = ((data[13] as u16) << 8) | data[12] as u16;
-        // Let's assume the client set the IP address and port number correctly
-        println!("Got {:?}, protocol = {}.{}, client = {}.{}.{}.{}, IP = {}.{}.{}.{}, port = {}",
-            ServerCommand::SendVersion,
-            protocol_major, protocol_minor,
-            client_major, client_minor, client_release, client_build,
-            ip[0], ip[1], ip[2], ip[3], port);
-        Ok(())
-    }
-}
-
-fn handle_message(client: &mut std::net::TcpStream, code: u8, data: &[u8]) -> Result<(), String>
-{
-    match get_command(code)
-    {
-        None => { eprintln!("Got unknown opcode {}", code); },
-        Some(cmd) => {
-            match cmd
-            {
-                ServerCommand::RawText => { println!("Got {:?}: {}", cmd, to_utf8(data)); },
-                ServerCommand::SendOpeningScreen => {
-                    println!("Got {:?}", cmd);
-                    // Looks like this may be ignored by the official server, but here we go:
-                    send_text(client, ChatChannel::Server, "Welcome to the EL Toy Server!")?;
-                },
-                ServerCommand::SendVersion => { handle_version(data)?; },
-                ServerCommand::HeartBeat => { println!("Got {:?}", cmd); },
-                ServerCommand::Invalid => { println!("Got {:?} {}", cmd, code); }
-            }
-        }
-    }
-    Ok(())
-}
-
-fn handle_packets(client: &mut std::net::TcpStream, buffer: &mut [u8]) -> Result<usize, String>
-{
-    let mut offset = 0;
-    while offset + 3 <= buffer.len()
-    {
-        let rem = &buffer[offset..];
-        let msg_len = 2 + ((rem[2] as usize) << 8) + rem[1] as usize;
-        if msg_len < 3 || msg_len > buffer.len()
-        {
-            return Err(String::from("Invalid message length"));
-        }
-        if msg_len > rem.len()
-        {
-            break;
-        }
-
-        let code = rem[0];
-        let data = &rem[3..msg_len];
-        handle_message(client, code, data)?;
-
-        offset += msg_len;
-    }
-    let new_size = buffer.len() - offset;
-    buffer.copy_within(offset.., 0);
-
-    Ok(new_size)
-}
-
-fn handle_client(mut client: std::net::TcpStream)
-{
-    let mut buffer = [0u8; 8192];
-    let mut buffer_used = 0;
-    loop
-    {
-        match client.read(&mut buffer[buffer_used..])
-        {
-            Ok(0) => {
-                println!("Client shut down the connection");
-                break;
-            },
-            Ok(nr_bytes) => {
-                buffer_used += nr_bytes;
-                match handle_packets(&mut client, &mut buffer[..buffer_used])
-                {
-                    Ok(len) => { buffer_used = len; },
-                    Err(err) => { eprintln!("Protocol error: {}", err); break; }
-                }
-            },
-            Err(err) => {
-                eprintln!("Failed to read from client socket: {}", err);
-                break;
-            }
-        }
-    }
-}
-
 fn main()
 {
+    let mut ctx = SslConnector::builder(SslMethod::tls()).unwrap();
     match std::net::TcpListener::bind("localhost:2121")
     {
         Ok(listener) => {
@@ -268,9 +312,9 @@ fn main()
             {
                 match stream
                 {
-                    Ok(client) => {
-                        println!("Got new client from {:?}", client.peer_addr());
-                        std::thread::spawn(|| { handle_client(client); });
+                    Ok(socket) => {
+                        println!("Got new client from {:?}", socket.peer_addr());
+                        std::thread::spawn(|| { Client::new(socket).handle_traffic(); });
                     },
                     Err(err) => {
                         eprintln!("Failed to accept a client: {}", err);
